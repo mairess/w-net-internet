a826bf8ade12bf6ff4a445e88dcb87ea3d9ff405
mairess <trabalho.maires@gmail.com>
2024-06-30 17:07:16 -0300

    refactor: remove address form customerDto

--- src/main/java/com/maires/wnet/controller/dto/CustomerDto.java
+++ src/main/java/com/maires/wnet/controller/dto/CustomerDto.java
@@ -1,14 +1,11 @@
 package com.maires.wnet.controller.dto;
 
-import com.maires.wnet.entity.Address;
 import com.maires.wnet.entity.Customer;
-import java.util.List;
 
 /**
  * The addressType Customer dto.
  */
 public record CustomerDto(Long id, String name, String cpf, String phone, String email,
-                          List<Long> addresses,
                           String registrationDate) {
 
   /**
@@ -24,7 +21,6 @@ public record CustomerDto(Long id, String name, String cpf, String phone, String
         customer.getCpf(),
         customer.getPhone(),
         customer.getEmail(),
-        customer.getAddresses().stream().map(Address::getId).toList(),
         customer.getRegistrationDate()
     );
   }
