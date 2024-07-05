5b0789cb991f1f7423d429f439019ffc3f4f3bda
mairess <trabalho.maires@gmail.com>
2024-07-05 17:15:47 -0300

    feat: route PUT /installations/installationId

--- src/main/java/com/maires/wnet/controller/InstallationController.java
+++ src/main/java/com/maires/wnet/controller/InstallationController.java
@@ -1,12 +1,19 @@
 package com.maires.wnet.controller;
 
+import com.maires.wnet.controller.dto.InstallationCreationDto;
 import com.maires.wnet.controller.dto.InstallationDto;
 import com.maires.wnet.service.InstallationService;
+import com.maires.wnet.service.exception.EquipmentAlreadyAssociatedException;
+import com.maires.wnet.service.exception.EquipmentNotFoundException;
 import com.maires.wnet.service.exception.InstallationNotFoundException;
+import com.maires.wnet.service.exception.PlanNotFoundException;
+import com.maires.wnet.service.exception.TechnicianNotFoundException;
 import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -54,6 +61,39 @@ public class InstallationController {
     return InstallationDto.fromEntity(installationService.findInstallationById(installationId));
   }
 
+  /**
+   * Update installation installation dto.
+   *
+   * @param installationId          the installation id
+   * @param installationCreationDto the installation creation dto
+   * @return the installation dto
+   * @throws InstallationNotFoundException       the installation not found exception
+   * @throws EquipmentAlreadyAssociatedException the equipment already associated exception
+   * @throws EquipmentNotFoundException          the equipment not found exception
+   * @throws TechnicianNotFoundException         the technician not found exception
+   * @throws PlanNotFoundException               the plan not found exception
+   */
+  @PutMapping("/{installationId}")
+  public InstallationDto updateInstallation(
+      @PathVariable Long installationId,
+      @RequestBody InstallationCreationDto installationCreationDto
+  ) throws
+      InstallationNotFoundException,
+      EquipmentAlreadyAssociatedException,
+      EquipmentNotFoundException,
+      TechnicianNotFoundException,
+      PlanNotFoundException {
+
+    return InstallationDto.fromEntity(
+        installationService.updateInstallation(
+            installationId,
+            installationCreationDto.planId(),
+            installationCreationDto.technicianId(),
+            installationCreationDto.equipmentIds()
+        )
+    );
+  }
+
 
   /**
    * Remove installation by id installation dto.
@@ -67,6 +107,5 @@ public class InstallationController {
       throws InstallationNotFoundException {
     return InstallationDto.fromEntity(installationService.removeInstallationById(installationId));
   }
-
 
 }
