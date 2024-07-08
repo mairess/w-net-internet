25c110b9c0aa80900e39b5746f4d748bb81b7fb9
mairess <trabalho.maires@gmail.com>
2024-07-08 12:30:56 -0300

    feat: add forgotten PutMapping

--- src/main/java/com/maires/wnet/controller/UserController.java
+++ src/main/java/com/maires/wnet/controller/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -106,6 +107,7 @@ public class UserController {
    * @return the user dto
    * @throws UserNotFoundException the user not found exception
    */
+  @PutMapping("/{userId}")
   @PreAuthorize("hasAuthority('ADMIN')")
   public UserDto updateUser(@PathVariable Long userId, UserCreationDto userCreationDto
   ) throws UserNotFoundException {
