3ac4fb7cad29d0c9c16aaaae18eb32064fe9b337
mairess <trabalho.maires@gmail.com>
2024-07-09 16:59:43 -0300

    chore: add kafka and mail to implement messaging

---
+++ src/main/java/com/maires/wnet/controller/dto/MessagingNewInstallationDto.java
@@ -0,0 +1,8 @@
+package com.maires.wnet.controller.dto;
+
+/**
+ * The type Messaging new installation dto.
+ */
+public record MessagingNewInstallationDto(String customerMail, String customerName) {
+
+}
